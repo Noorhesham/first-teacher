bed63d68e445faa0c167ac330c2ad305ca05d86c
noor <noordragon2004@gmail.com>
2024-04-16 03:37:45 -0700

    emew

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
+  const parallaxContainer = document.getElementById("parallaxContainer");
+  const parallaxContainer1 = document.getElementById("parallaxContainer1");
+  const parallaxContainer2 = document.getElementById("parallaxContainer2");
+  const parallaxImage = document.getElementById("parallaxImage");
+
+  window.addEventListener("scroll", function () {
+    const scrollY = window.scrollY;
+    parallaxContainer.style.transform = `translateY(-${scrollY * 1}px)`;
+    parallaxContainer1.style.transform = `translateY(-${scrollY * 0.8}px)`;
+    parallaxContainer2.style.transform = `translateY(-${scrollY * 0.8}px)`;
+    parallaxImage.style.top = `-${scrollY}px`;
+  });
+});
+document.addEventListener("DOMContentLoaded", function () {
   const navbar = document.getElementById("navbar");
 
   window.addEventListener("scroll", function () {
