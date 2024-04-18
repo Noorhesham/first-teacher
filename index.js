da0701bdb7f966191e0f6e103374a266d1deabb3
noor <noordragon2004@gmail.com>
2024-04-18 05:38:19 -0700

    emew

--- index.js
+++ index.js
@@ -5,45 +5,45 @@ document.addEventListener("DOMContentLoaded", function () {
 
   window.addEventListener("scroll", function () {
     const scrollY = window.scrollY;
-    parallaxContainers.forEach(container => {
+    parallaxContainers.forEach((container) => {
       container.style.transform = `translateY(-${scrollY * 0.8}px)`;
     });
-    parallaxImage.style.top = `-${scrollY}px`;
+    if (parallaxImage) parallaxImage.style.top = `-${scrollY}px`;
   });
 
   // Avatar menu
-  const avatar = document.querySelector('.avatar');
-  const menu = document.getElementById('menu');
+  const avatar = document.querySelector(".avatar");
+  const menu = document.getElementById("menu");
 
-  avatar.addEventListener('click', function(event) {
+  avatar.addEventListener("click", function (event) {
     event.stopPropagation();
-    menu.style.display = (menu.style.display === 'block') ? 'none' : 'block';
+    menu.style.display = menu.style.display === "block" ? "none" : "block";
   });
 
-  document.addEventListener('click', function(event) {
+  document.addEventListener("click", function (event) {
     if (event.target !== avatar && event.target !== menu && !menu.contains(event.target)) {
-      menu.style.display = 'none';
+      menu.style.display = "none";
     }
   });
 
   // Accordion
-  const accordionItems = document.querySelectorAll('.accordion-item');
+  const accordionItems = document.querySelectorAll(".accordion-item");
 
-  accordionItems.forEach(item => {
-    item.addEventListener('click', function() {
+  accordionItems.forEach((item) => {
+    item.addEventListener("click", function () {
       const accordionContent = this.nextElementSibling;
       const accordion = this.parentElement;
 
-      if (accordion.classList.contains('open')) {
-        accordion.classList.remove('open');
-        accordion.classList.add('close');
+      if (accordion.classList.contains("open")) {
+        accordion.classList.remove("open");
+        accordion.classList.add("close");
         setTimeout(() => {
-          accordionContent.style.display = 'none';
-          accordion.classList.remove('close');
+          accordionContent.style.display = "none";
+          accordion.classList.remove("close");
         }, 300); // Wait for the animation to complete
       } else {
-        accordion.classList.add('open');
-        accordionContent.style.display = 'block';
+        accordion.classList.add("open");
+        accordionContent.style.display = "block";
       }
     });
   });
@@ -53,7 +53,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
   window.addEventListener("scroll", function () {
     navbar.classList.toggle("scrolled", window.scrollY > 100);
-    menu.style.display = 'none';
+    menu.style.display = "none";
   });
 
   // Input field animations
